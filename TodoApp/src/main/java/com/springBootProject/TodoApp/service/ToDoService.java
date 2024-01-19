f9ea955c992bff3f8e2ffba7f54f8aa13f82af6e
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-01-19 12:14:40 +0300

    Created the todo service and  repository

---
+++ TodoApp/src/main/java/com/springBootProject/TodoApp/service/ToDoService.java
@@ -0,0 +1,42 @@
+package com.springBootProject.TodoApp.service;
+
+
+import com.springBootProject.TodoApp.Repository.ToDoRepository;
+import com.springBootProject.TodoApp.exceptions.UserNotFoundException;
+import com.springBootProject.TodoApp.models.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ToDoService {
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    public List<Todo> getAllToDo(){
+        return toDoRepository.findAll();
+    }
+
+    public Todo getToDoById(long id) {
+        return toDoRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(
+                        "User of ID NO " + id + " not found"
+                ));
+    }
+    public Todo saveToDo(Todo todo){
+        return toDoRepository.save(todo);
+    }
+    public Todo updateTodo(Todo todo){
+
+        Todo existingTodo = toDoRepository.findById(todo.getId())
+                .orElseThrow(() -> new UserNotFoundException(
+                        "User not found"
+                ));
+        existingTodo.setTitle(todo.getTitle());
+        existingTodo.setDate(todo.getDate());
+        existingTodo.setStatus(todo.getStatus());
+
+        return toDoRepository.save(existingTodo);
+    }
+}
